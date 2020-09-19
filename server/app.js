b8c2ecdd5e1e0608d31ab1183786a8d65086d25c
Ally <el.ally741@gmail.com>
2020-09-19 11:52:24 +0200

    feature(buses): crud operation for buses
    Create the controller of the endpoint
    Create the router of the endpoint
    Call the router in the app
    
    [(footer) #rWXS2NYV]
    
    chore(eslint): eslint configuration
    
    Setting up the eslint
    
    [(finished) #rWXS2NYV]
    
    chore(swagger): swagger set up
    
    Creation of the swagger.json file
    Setting up swagger.json
    
    [(finished) #rWXS2NYV)]
    
    fix(fix setting): setting up
    
    fix(setting): setting up
    
    Add a last commit to squash
    
    [(finished) #rWXS2NYV]
    
    feature(setting): setting up the node.js
    
    Fixing the feedback from our TTL
    
    [(finished) #rWXS2NYV]
    
    fix(swagger): changing to .js file
    
    Creating a swagger folder
    Creating a index.js file in swagger folder
    Creating a welcome.swagger.js file in swagger folder
    
    [(in-progress) #rWXS2NYV]
    
    bug(swagger): swagger documentation
    
    Changing the swagger documentation from .json file to .js file
    
    [(finished) #rWXS2NYV]

--- server/app.js
+++ server/app.js
@@ -2,27 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import i18n from 'i18n';
-import routes from './routers';
-import router from './routers/bus.routers';
+import routes from '../src/routers';
 
 dotenv.config();
 
 const app = express();
-app.use(i18n.init);
+
 app.use(cors());
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
+
 app.use(routes);
 
-app.use(router);
-
-const port = process.env.PORT || 3020;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(port);
+  console.log(`The server is running on ${port}`);
 });
 
 export default app;
