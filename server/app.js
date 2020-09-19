87c619358e03f41323e4bcc46f9718ba53cc64dd
Ally <el.ally741@gmail.com>
2020-09-19 16:09:13 +0200

    chore(swagger): swagger set up
    Creation of the swagger.json file
    Setting up swagger.json
    
    [(finished) #rWXS2NYV)]

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
 import routes from './routers/routers';
+import swagger from '../swagger.json';
 
 dotenv.config();
 
@@ -14,6 +17,10 @@ app.use(bodyParser.json());
 
 app.use('/', routes);
 
+app.use(cors());
+
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagger));
 
 
 const port = process.env.PORT || 4000;
