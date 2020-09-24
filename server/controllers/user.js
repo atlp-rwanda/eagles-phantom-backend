3bb48eb33593dc1512f1b16c061acdf60252fea5
Ntwali-Josue <josh1234king@gmail.com>
2020-09-24 12:47:23 +0200

    bug(ESlint): solve eslint errors [fixes #8]

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 /* eslint-disable linebreak-style */
-/* eslint-disable arrow-body-style */
-const welcome = (req, res) => {
-  return res.status(200).json({
-    status: 200,
-    message: 'Welcome to phantom, a platform to facilitate the transportation mode in town!.',
-  });
-};
-=======
 const welcome = (req, res) => res.status(200).json({
   status: 200,
   message: 'Welcome to phantom, a platform to facilitate the transportation mode in town!.',
 });
->>>>>>> ecafcd9a0060bb44c2a4e643191fb9605847111f
 
 export default welcome;
--- server/routers/routers.js
+++ server/routers/routers.js
@@ -1,3 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable linebreak-style */
 import express from 'express';
 import welcome from '../controllers/user';
 import swagger from '../swagger/index';
