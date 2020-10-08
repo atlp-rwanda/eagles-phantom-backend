9a7ab818f6afaf9ec2a174f5ffcf97e7400d61ee
Josh <58233753+Ntwali-Josue@users.noreply.github.com>
2020-10-08 12:16:33 +0200

    Update README.md
    fix(heroku): heroku pipeline
    
    deleting and putting .nyc_output in gitignore
    
    [(finishes) #1oYL5X1g]
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    fix(travis): solve travis error

--- src/test/test.js
+++ src/test/test.js
@@ -1,19 +1,18 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 
 import app from '../app';
 
-const { expect } = pkg;
 chai.use(chaiHttp);
 
 describe('Server!', () => {
-  it(' the api testing', (done) => {
+  it('the api testing', (done) => {
     chai
       .request(app)
       .get('/')
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body.message).to.equals('Welcome to phantom, a platform to facilitate the transportation mode in town!.');
+        expect(res.body.message).to.equals('Welcome to phantom!.');
         done();
       });
   });
