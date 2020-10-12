956d390d1b4f28fd6fbcb09a987af31438a3a0fe
Mugema123 <mugemaleo@gmail.com>
2020-10-12 07:05:36 +0200

    fix(error):fix-errors

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import welcome from '../controllers/user';
+<<<<<<< HEAD
 // import translator from '../languages/config.js';
+=======
+import login from '../controllers/controller';
+import logout from '../controllers/logout';
+import getInfo from '../controllers/info';
+import auth from '../Middlewares/auth';
+>>>>>>> 14f988b... ft(logout):log-out-users-#qgAq5WKe
 import swagger from '../swagger/index';
 
 const router = express.Router();
@@ -15,12 +22,39 @@ const router = express.Router();
 *     responses:
 *       200:
 *         description:Welcome to phantom, a platform to facilitate the transportation mode in town!.
+/login:
+*   post:
+*     description: Display welcome message
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description:Welcome to phantom, a platform to facilitate the transportation mode in town!.
+/logout:
+*   get:
+*     description: Display welcome message
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description:Welcome to phantom, a platform to facilitate the transportation mode in town!.
+/drivers/info:
+*   get:
+*     description: Display welcome message
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description:Welcome to phantom, a platform to facilitate the transportation mode in town!.
 */
+
+
 
 router.get('/', welcome);
 
+router.post('/login', login);
+router.get("/logout",auth.auth,logout);
+router.get("/drivers/info",auth.auth,getInfo);
 router.use('/api-docs', swagger);
-
-// router.get('/?lang=fr', translator);
 
 export default router;
